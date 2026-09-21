499ff5d9f772e18e14d8cbe8685ed3ab42d63375
Lendl Verschoor <lendlverschoor@hotmail.com>
2026-09-21 20:47:42 +0200

    fix unit test

--- app/src/test/java/com/quietgrid/app/games/wordguess/WordGuessLocaleTest.kt
+++ app/src/test/java/com/quietgrid/app/games/wordguess/WordGuessLocaleTest.kt
@@ -5,7 +5,7 @@ import org.junit.Test
 
 class WordGuessLocaleTest {
     @Test
-    fun `WORDGUESS_SUPPORTED_LOCALES matches the app's six shipped locales`() {
-        assertTrue(WORDGUESS_SUPPORTED_LOCALES == setOf("en", "de", "es", "fr", "nl", "pl"))
+    fun `WORDGUESS_SUPPORTED_LOCALES matches the app's seven shipped locales`() {
+        assertTrue(WORDGUESS_SUPPORTED_LOCALES == setOf("en", "de", "es", "fr", "nl", "pl", "pt"))
     }
 }
