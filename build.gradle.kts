704fbda8d0e0708fa4f7577341aa37441aa52a91
Lendl Verschoor <lendlverschoor@hotmail.com>
2026-09-23 21:49:08 +0200

    downgrade kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("com.android.application") version "9.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.4.20" apply false
-    id("org.jetbrains.kotlin.jvm") version "2.4.20" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.4.20" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.10" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.4.10" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.4.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
     id("com.android.compose.screenshot") version "0.0.1-alpha15" apply false
     id("com.google.dagger.hilt.android") version "2.60.1" apply false
 }
